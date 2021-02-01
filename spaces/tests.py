38e8c1413454c177aefd301a4925ff654fe28d0b
jmyoon <wjdals.yoon14@gmail.com>
2021-02-01 19:29:35 +0900

    Add : spacedetailview

--- spaces/tests.py
+++ spaces/tests.py
@@ -12,13 +12,12 @@ from spaces.models      import (Space, Location, Image, Bedroom,
 client = Client()
 class SpaceListViewTest(TestCase):
     def setUp(self):
-        Country.objects.create(
+        country = Country.objects.create(
             name = "한국"
         )
-        country_id = Country.objects.get(name="한국").id
 
-        User.objects.create(
-            country_id     = country_id,
+        user = User.objects.create(
+            country_id     = country.id,
             phone          = None,
             name           = "test",
             password       = "test",
@@ -28,58 +27,51 @@ class SpaceListViewTest(TestCase):
             profile_photo  = None,
             is_email_valid = 0
         )
-        user_id = User.objects.get(name="test").id
 
-        Host.objects.create(
-            user_id       = user_id,
+        host = Host.objects.create(
+            user_id       = user.id,
             profile_photo = "host_photo",
-            id_card_photo = None
+            id_card_photo = "id_card_url"
         )
-        host_id = Host.objects.get(profile_photo="host_photo").id
 
-        Property.objects.create(
+        space_property = Property.objects.create(
             name = "아파트"
         )
-        property_id = Property.objects.get(name="아파트").id
 
-        SubProperty.objects.create(
+        subProperty = SubProperty.objects.create(
             name              = "아파트",
-            space_property_id = property_id
+            space_property_id = space_property.id
         )
-        subproperty_id = SubProperty.objects.get(name="아파트").id
 
-        PlaceType.objects.create(
-            name = "다인실", 
+        placeType = PlaceType.objects.create(
+            name  = "다인실", 
         )
-        placetype_id = PlaceType.objects.get(name="다인실").id
 
-        Space.objects.create(
-            host_id         = host_id,
+        space = Space.objects.create(
+            host_id         = host.id,
             name            = "숙소이름",
             price           = "10000.00",
             description     = "숙소 설명",
-            place_type_id   = placetype_id,
-            sub_property_id = subproperty_id,
+            place_type_id   = placeType.id,
+            sub_property_id = subProperty.id,
             max_people      = 5,
             bathroom        = 1,
             rating          = "5.00"
         )
-        space_id = Space.objects.get(name="숙소이름").id
 
-        Option.objects.create(
+        option = Option.objects.create(
             name     = "와이파이",
             icon_url = "wifi_url",
         )
-        option_id = Option.objects.get(name="와이파이").id
 
-        SpaceOption.objects.create(
-            space_id  = space_id,
-            option_id = option_id
+        spaceOption = SpaceOption.objects.create(
+            space_id  = space.id,
+            option_id = option.id
         )
 
-        Location.objects.create(
-            space_id       = space_id,
-            country_id     = country_id,
+        locaion = Location.objects.create(
+            space_id       = space.id,
+            country_id     = country.id,
             region         = "서울시",
             city           = "강남구",
             address        = "삼성동 테헤란로",
@@ -88,37 +80,34 @@ class SpaceListViewTest(TestCase):
             longitude      = "127.054067"
         )
 
-        Image.objects.create(
-            space_id = space_id,
+        image = Image.objects.create(
+            space_id = space.id,
             url      = "space_image_url"
         )
        
-        BedType.objects.create(
+        bedType = BedType.objects.create(
            name   = "퀸 침대"  
         )
-        bedtype_id = BedType.objects.get(name="퀸 침대").id
 
-        Bedroom.objects.create(
+        bedroom = Bedroom.objects.create(
            name     = "침실",
-           space_id = space_id
+           space_id = space.id
         )
-        bedroom_id = Bedroom.objects.get(name="침실").id
 
-        BedroomBed.objects.create(
+        bedRoom = BedroomBed.objects.create(
         
-           bedroom_id = bedroom_id,
-           bed_type_id = bedtype_id,
+           bedroom_id  = bedroom.id,
+           bed_type_id = bedType.id,
            quantity    = 5
         )
 
-        Tag.objects.create(
+        tag = Tag.objects.create(
            name = "test"
         )
-        tag_id = Tag.objects.get(name="test").id
 
-        SpaceTag.objects.create(
-           space_id = space_id,
-           tag_id   = tag_id 
+        spaceTag = SpaceTag.objects.create(
+           space_id = space.id,
+           tag_id   = tag.id 
         )
 
     def tearDown(self):
@@ -146,6 +135,7 @@ class SpaceListViewTest(TestCase):
         {
     "results": [
         {
+            "id":3,
             "name": "숙소이름",
             "place_type": "다인실",
             "city": [
@@ -165,12 +155,8 @@ class SpaceListViewTest(TestCase):
             "rating": "5.00",
             "price": "10000.00",
             "space_tag": ["test"],
-            "latitude": [
-                "37.506267"
-            ],
-            "longitude": [
-                "127.054067"
-            ]
+            "latitude": ["37.506267"],
+            "longitude": ["127.054067"]
         }
     ]
         }
@@ -196,5 +182,178 @@ class SpaceListViewTest(TestCase):
         Country.objects.all().delete()
 
         response = client.get("/spaces")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),{"message" : "SPACE_DOES_NOT_EXIST"})
+
+
+client = Client()
+class SpaceDetailViewTest(TestCase):
+    def setUp(self):
+        country = Country.objects.create(
+            name = "한국"
+        )
+
+        user = User.objects.create(
+            country_id     = country.id,
+            phone          = None,
+            name           = "test",
+            password       = "test",
+            gender         = None,
+            birthdate      = None,
+            email          = "test@test.com",
+            profile_photo  = None,
+            is_email_valid = 0
+        )
+
+        host = Host.objects.create(
+            user_id       = user.id,
+            profile_photo = "host_photo",
+            id_card_photo = "id_card_url"
+        )
+
+        space_property = Property.objects.create(
+            name = "아파트"
+        )
+
+        subProperty = SubProperty.objects.create(
+            name              = "아파트",
+            space_property_id = space_property.id
+        )
+
+        placeType = PlaceType.objects.create(
+            name  = "다인실", 
+        )
+
+        space = Space.objects.create(
+            host_id         = host.id,
+            name            = "숙소이름",
+            price           = "10000.00",
+            description     = "숙소 설명",
+            place_type_id   = placeType.id,
+            sub_property_id = subProperty.id,
+            max_people      = 5,
+            bathroom        = 1,
+            rating          = "5.00"
+        )
+
+        option = Option.objects.create(
+            name     = "와이파이",
+            icon_url = "wifi_url",
+        )
+
+        spaceOption = SpaceOption.objects.create(
+            space_id  = space.id,
+            option_id = option.id
+        )
+
+        locaion = Location.objects.create(
+            space_id       = space.id,
+            country_id     = country.id,
+            region         = "서울시",
+            city           = "강남구",
+            address        = "삼성동 테헤란로",
+            address_detail = "427",
+            latitude       = "37.506267",
+            longitude      = "127.054067"
+        )
+
+        image = Image.objects.create(
+            space_id = space.id,
+            url      = "space_image_url"
+        )
+       
+        bedType = BedType.objects.create(
+           name   = "퀸 침대"  
+        )
+
+        bedroom = Bedroom.objects.create(
+           name     = "침실",
+           space_id = space.id
+        )
+
+        bedRoom = BedroomBed.objects.create(
+        
+           bedroom_id  = bedroom.id,
+           bed_type_id = bedType.id,
+           quantity    = 5
+        )
+
+        tag = Tag.objects.create(
+           name = "test"
+        )
+
+        spaceTag = SpaceTag.objects.create(
+           space_id = space.id,
+           tag_id   = tag.id 
+        )
+
+    def tearDown(self):
+        Space.objects.all().delete(),
+        PlaceType.objects.all().delete(),
+        Property.objects.all().delete(),
+        SubProperty.objects.all().delete(),
+        Option.objects.all().delete(),
+        SpaceOption.objects.all().delete(),
+        Location.objects.all().delete(),
+        Image.objects.all().delete(),
+        Bedroom.objects.all().delete(),
+        BedroomBed.objects.all().delete(),
+        BedType.objects.all().delete(),
+        Tag.objects.all().delete(),
+        SpaceTag.objects.all().delete(),
+        User.objects.all().delete(),
+        Host.objects.all().delete(),
+        Country.objects.all().delete()
+
+    def test_get_space_detail_view(self):
+        response = client.get('/spaces/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+        {
+    "detail_space": [
+       {
+         "id": 1,
+         "name": "숙소이름",
+         "address": "서울시 강남구 삼성동 테헤란로 427",
+         "description": "숙소 설명",
+         "space_image": [
+           "space_image_url"
+         ],
+         "host": "test",
+         "host_profile": "host_photo",
+         "propert_name": "아파트",
+         "max_people": 5,
+         "price": "10000.00",
+         "rating": "5.00",
+         "latitude": "37.506267",
+         "longitude": "127.054067",
+         "bedroom": [
+           {
+             "name": "침실",
+             "bed_type": [
+               "퀸 침대"
+             ],
+             "bed_quantity": [
+               5
+             ]
+           }
+         ],
+         "bedroom_quantity": 1,
+         "bed_quantity_sum": 5,
+         "bathroom_quantity": 1,
+         "option": [
+           {
+             "name": "와이파이",
+             "url": "wifi_url"
+           }
+         ]
+       }
+     ]
+}
+
+    )  
+
+    def test_get_space_detail_view_error(self):
+        response = client.get("/spaces/3333")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),{"message" : "SPACE_DOES_NOT_EXIST"})
